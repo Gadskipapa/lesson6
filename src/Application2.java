5668c9e640881d156732281aa1964adb22ddc99e
Art <atanatos@ya.ru>
2018-11-02 21:28:50 +0300

    6

---
+++ src/Application2.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Application2 {
+    public static void main(String[] args) {
+        int[] a = {9, 8, 6, -7, 3, 4};
+    }
+
+    public static int getMaaxFromArray(int[]array) {
+        int indexOfMax = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[indexOfMax]) {
+                indexOfMax = i;
+            }
+        }
+        return array[indexOfMax];
+    }
+
+
+}
